c0f5fd2bf016784cfcbe408577027eaee915ad89
LAPTOP-4GUOFFNG\pavel <pavelkonecny87@gmail.com>
2022-01-11 21:15:02 +0100

    Initial commit

---
+++ src/Containers/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyledApp } from './App.style';
+
+function App() {
+  // Load data
+
+  // Render app
+  return (
+    <StyledApp>
+      <p>Lorem ipsum</p>
+    </StyledApp>
+  );
+}
+
+export default App;
